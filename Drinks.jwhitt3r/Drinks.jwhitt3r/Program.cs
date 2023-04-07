4751071874a9e9c3c6be7684d48dd44c9bddf5d9
jwhitter <jackwhitterjones@gmail.com>
2023-04-07 01:31:54 +0100

    Setup a generic API data requester within RetrieveData.cs. Got a simple user program consuming the class and then writing data out based on the feedback and declaration of a record. In addition, this is using the built in json classes, not NewtonSoft. Painful to say the least!

--- Drinks.jwhitt3r/Drinks.jwhitt3r/Program.cs
+++ Drinks.jwhitt3r/Drinks.jwhitt3r/Program.cs
@@ -1,2 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Drinks.jwhitt3r;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace Drinks.jwhitt3r {
+    class Program {
+
+        public record DataRecord(
+            [property: JsonPropertyName("id")] int DataOne,
+            [property: JsonPropertyName("name")] string DataTwo,
+            [property: JsonPropertyName("tagline")] string DataThree,
+            [property: JsonPropertyName("ingredients")] Dictionary<string, JsonNode> DataFour
+            );
+        public static void Main() {
+            Console.WriteLine("\nBrewDog Beer Menu");
+            RetrieveData myData = new("https://api.punkapi.com/v2/beers");
+            myData.GetDataAsync().Wait();
+            foreach (var item in myData.items)
+            {
+                Console.WriteLine($"ID: {item.DataOne}");
+                Console.WriteLine($"Name: {item.DataTwo}");
+                Console.WriteLine($"Tagline: {item.DataThree}");
+                var dat = item.DataFour.ToList();
+                foreach(var d in dat)
+                {
+                    Console.WriteLine($"{d.Key}");
+                    if(d.Value.GetType() == typeof(JsonArray))
+                    {
+                        foreach(var e in d.Value.AsArray())
+                        {
+                            Console.Write($"\t{e["name"]} - {e["amount"]["value"]} in {e["amount"]["unit"]}\n");
+                        }
+                        
+                    } else
+                    {
+                        Console.Write($"\t{d.Value}");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Please enter an ID number you would like to query");
+            string userInput = Console.ReadLine();
+            myData.SearchByID(userInput).Wait();
+            foreach (var item in myData.items)
+            {
+                Console.WriteLine($"ID: {item.DataOne}");
+                Console.WriteLine($"Name: {item.DataTwo}");
+                Console.WriteLine($"Tagline: {item.DataThree}");
+                var dat = item.DataFour.ToList();
+                foreach (var d in dat)
+                {
+                    Console.WriteLine($"{d.Key}");
+                    if (d.Value.GetType() == typeof(JsonArray))
+                    {
+                        foreach (var e in d.Value.AsArray())
+                        {
+                            Console.Write($"\t{e["name"]} - {e["amount"]["value"]} in {e["amount"]["unit"]}\n");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.Write($"\t{d.Value}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
---
+++ Drinks.jwhitt3r/Drinks.jwhitt3r/RetrieveData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Drinks.jwhitt3r
+{
+    /// <summary>
+    /// RetrieveData gathers all the data from an API of the users choice.
+    /// </summary>
+    class RetrieveData
+    {
+        public string ApiUrl { get; set; }
+        public List<Program.DataRecord> items;
+        public RetrieveData(string apiUrl) 
+        {
+            this.ApiUrl = apiUrl;
+            this.items = new List<Program.DataRecord>();
+            
+        }
+
+        public async Task<List<Program.DataRecord>> ProcessApiDataAsync(HttpClient client, string id = "")
+        {
+            await using Stream stream =
+                await client.GetStreamAsync($"{this.ApiUrl}/{id}");
+            var items =
+                await JsonSerializer.DeserializeAsync<List<Program.DataRecord>>(stream);
+            return items ?? new();
+        }
+
+        public async Task GetDataAsync(string userInput = "")
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+            this.items = await ProcessApiDataAsync(client, userInput);
+        }
+
+        internal async Task SearchByID(string userInput)
+        {
+            await GetDataAsync(userInput);
+        }
+    }
+
+}
