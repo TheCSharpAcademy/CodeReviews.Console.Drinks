b7896a843e30e87da8f536b579166e746ad48da9
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-04-14 14:02:37 +0200

    Add the folder back

---
+++ Gautatyr.Drinks/DrinksInfos/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Metadata;
+using static DrinksInfos.APIManager;
+
+namespace DrinksInfos;
+
+public static class Helpers
+{
+    public static void DisplayError(string error)
+    {
+        Console.WriteLine($"\n|---> Error: {error} <---|\n");
+    }
+
+    public static Categories[] GetSequencedCategoriesList()
+    {
+        var categories = GetCategoriesAsync();
+        int id = 1;
+
+        foreach (var category in categories)
+        {
+            category.id = id;
+            id++;
+        }
+
+        return categories;
+    }
+
+    public static Drink[] GetSequencedDrinkList(string categoryName)
+    {
+        var drinks = GetDrinksAsync(categoryName);
+        int id = 1;
+
+        foreach (var drink in drinks)
+        {
+            drink.Id = id.ToString();
+            id++;
+        }
+
+        return drinks;
+    }
+}
