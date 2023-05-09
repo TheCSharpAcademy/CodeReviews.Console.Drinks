6b3b3a00fd752c28db10a7bf3c1ba4371d08aa16
heartingNinja <neilfosteronly@gmail.com>
2023-05-09 23:13:56 +0700

    following tutorial

---
+++ Drinks.heartingNinja/Drinks/Models/Drinks.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace drinks_info.Models;
+
+public class Drinks
+{
+    [JsonProperty("drinks")]
+    public List<Drink> DrinkList { get; set; }
+}
+
+public class Drink
+{
+    public string idDrink { get; set; }
+    public string strDrink { get; set; }
+}
---
+++ Drinks.heartingNinja/Drinks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace drinks_info;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello World");
+        UserInput userInput = new UserInput();
+        userInput.GetCategoriesInput();
+        Console.Read();
+    }
+}
