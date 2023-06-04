bf015c0ecc311caa1d7ef89ec5ef43a682c3f3fa
Vocal <vocalnight@protonmail.com>
2023-06-04 17:19:36 -0300

    project done

---
+++ Drinks.Vocalnight/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksInfo.Models
+{
+    public class Category : IDrinksJson
+    {
+        public string strCategory { get; set; }
+
+        public string GetName()
+        {
+            return strCategory;
+        }
+
+        public void ChangeName(string name)
+        {
+            strCategory = name;
+        }
+    }
+
+    public class Categories
+    {
+        [JsonProperty("drinks")]
+        public List<Category> CategoriesList { get; set; }
+    }
+}
---
+++ Drinks.Vocalnight/Models/DrinkModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksInfo
+{
+    public class DrinkModel
+    {
+        public string strDrink { get; set; }
+
+        public string strAlcoholic { get; set; }
+
+        public string strGlass { get; set; }
+
+        public string strInstructions { get; set; }
+
+    }
+
+    public class DrinkInformation
+    {
+        [JsonProperty("drinks")]
+
+        public List<DrinkModel> DrinkModel { get; set; }
+    }
+}
