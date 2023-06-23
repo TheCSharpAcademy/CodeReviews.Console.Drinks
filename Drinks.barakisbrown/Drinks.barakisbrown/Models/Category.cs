87e4f9de77c18b198be39d0056503e64457a25e9
Matthew Brown <matthew@lokislayer.com>
2023-06-23 17:37:45 -0500

    Model Folder

---
+++ Drinks.barakisbrown/Drinks.barakisbrown/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace Drinks.barakisbrown.Models;
+
+using Newtonsoft.Json;
+
+public class Category
+{
+	public Category()
+	{
+	}
+
+	public string strCategory { get; set; }
+}
+
+public class Categories
+{
+	[JsonProperty("drinks")]
+	public List<Category> CategoriesList { get; set; }
+}
+
---
+++ Drinks.barakisbrown/Drinks.barakisbrown/Models/Drinks.cs
@@ -0,0 +1,15 @@
+﻿namespace drinks_info.Models;
+
+using Newtonsoft.Json;
+
+public class Drinks
+{
+    [JsonProperty("drinks")]
+    public List<Drink> DrinksList { get; set; }
+}
+
+public class Drink
+{
+    public string idDrink { get; set; }
+    public string strSrink { get; set; }
+}
