89fc93f5e76d207772092037597d441600e22436
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-15 12:17:27 +0200

    feature[user interface]: create main menu

--- Drinks.MartinL-no/Drinks.MartinL-no/User Interface/UserInput.cs
+++ Drinks.MartinL-no/Drinks.MartinL-no/User Interface/UserInput.cs
@@ -10,5 +10,31 @@ internal class UserInput
 	{
         _controller = controller;
     }
+
+    public async Task Run()
+    {
+        var categories = (await _controller.GetCategories()).ToList();
+
+        while (true)
+        {
+            Console.Clear();
+
+            // Table to be added
+
+            var category = Helpers.Ask("Choose category: ");
+
+            if (categories.Exists(c => c.ToLower() == category.ToLower()))
+            {
+                ShowDrinksMenu(category);
+            }
+
+            else Helpers.ShowMessage("Invalid input, please try again");
+        }
+    }
+
+    private void ShowDrinksMenu(string category)
+    {
+        throw new NotImplementedException();
+    }
 }
 
