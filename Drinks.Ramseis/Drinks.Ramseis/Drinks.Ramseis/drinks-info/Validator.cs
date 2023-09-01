0a3641133360275d3bea9351156a9ce7bd64dee8
Schmit <nickvschmit@yahoo.com>
2023-09-01 08:08:34 -0700

    Initial

---
+++ Drinks.Ramseis/Drinks.Ramseis/Drinks.Ramseis/drinks-info/Validator.cs
@@ -0,0 +1,26 @@
+﻿namespace Drinks.Ramseis
+{
+    internal class Validator
+    {
+        public static bool IsStringValid(string input)
+        {
+            if (String.IsNullOrEmpty(input)) { return false; }
+            
+            foreach (char c in input)
+            {
+                if (!Char.IsLetter(c) && c != '/' && c != ' ') { return false; }
+            }
+            return true;
+        }
+
+        public static bool IsIdValid(string input)
+        {
+            if (String.IsNullOrEmpty(input)) { return false; }
+            foreach(char c in input)
+            {
+                if (!Char.IsDigit(c)) { return false; }
+            }
+            return true;
+        }
+    }
+}
