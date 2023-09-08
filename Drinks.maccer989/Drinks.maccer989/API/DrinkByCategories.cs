01427fa4a64cb4d46bf511022ad859cd2941e3de
maccer989 <132687553+maccer989@users.noreply.github.com>
2023-09-08 15:17:50 +0100

    Drinks.Maccer989
    Project Completed

---
+++ Drinks.maccer989/Drinks.maccer989/API/DrinkByCategories.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Drinks.maccer989
+{
+    public class DrinkByCategories
+    {
+        internal List<Drink> GetDrinksByCategory(string category)
+        {
+            var client = new RestClient("http://www.thecocktaildb.com/api/json/v1/1/");
+            var request = new RestRequest($"filter.php?c={HttpUtility.UrlEncode(category)}");
+
+            var response = client.ExecuteAsync(request);
+
+            List<Drink> drinks = new();
+
+            if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string rawResponse = response.Result.Content;
+
+                var serialize = JsonConvert.DeserializeObject<Drinks>(rawResponse);
+
+                drinks = serialize.DrinksList;
+
+                TableVisualisation.ShowTable(drinks, "Drinks Menu");
+
+                return drinks;
+
+            }
+
+            return drinks;
+
+        }
+    }
+}
---
+++ Drinks.maccer989/Drinks.maccer989/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Drinks.maccer989
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            UserInput userInput = new();
+            userInput.GetCategoriesInput();
+
+
+
+        }
+
+    }
+}
+
