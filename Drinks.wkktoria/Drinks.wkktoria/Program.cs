79319008c83ad521625d6006f7233be5493b4f38
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-17 21:21:45 +0200

    Add database connection and initialization

--- Drinks.wkktoria/Drinks.wkktoria/Program.cs
+++ Drinks.wkktoria/Drinks.wkktoria/Program.cs
@@ -1,4 +1,12 @@
-﻿using Drinks.wkktoria;
+﻿using System.Configuration;
+using Drinks.wkktoria;
 
-var userInput = new UserInput();
-userInput.GetCategoriesInput();
+var databaseName = ConfigurationManager.AppSettings.Get("DatabaseName");
+var databasePassword = ConfigurationManager.AppSettings.Get("DatabasePassword");
+var connectionString = $"Server=localhost,1433;User Id=sa;Password={databasePassword};TrustServerCertificate=true;";
+
+var databaseManager = new DatabaseManager(connectionString, databaseName!);
+databaseManager.Initialize();
+
+// var userInput = new UserInput();
+// userInput.GetCategoriesInput();
