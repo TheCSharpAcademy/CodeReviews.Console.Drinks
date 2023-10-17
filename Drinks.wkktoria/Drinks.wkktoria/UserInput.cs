da0beec3013f2c6483858a49a4bcfda15c35a948
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-17 20:05:24 +0200

    Refactor code

--- Drinks.wkktoria/Drinks.wkktoria/UserInput.cs
+++ Drinks.wkktoria/Drinks.wkktoria/UserInput.cs
@@ -7,29 +7,27 @@ internal class UserInput
 {
     internal void GetCategoriesInput()
     {
+        Console.Clear();
+
         var categories = DrinksService.GetCategories();
 
         if (categories == null) return;
 
-        var category = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("Choose category:")
-                .AddChoices(categories.Select(category => category.StrCategory).ToList()));
+        var category =
+            SelectionPrompt("Choose category:", categories.Select(category => category.StrCategory).ToList());
 
         GetDrinksInput(category);
     }
 
     private void GetDrinksInput(string category)
     {
+        Console.Clear();
+
         var drinks = DrinksService.GetDrinksByCategory(category);
 
         if (drinks == null) return;
 
-        var drinkName = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("Choose drink:")
-                .AddChoices(drinks.Select(drink => drink.StrDrink).ToList())
-        );
+        var drinkName = SelectionPrompt("Choose drink:", drinks.Select(drink => drink.StrDrink).ToList());
 
         var selectedDrink =
             drinks.Find(drink => drink.StrDrink.Equals(drinkName, StringComparison.InvariantCultureIgnoreCase));
@@ -39,5 +37,13 @@ internal class UserInput
         Console.WriteLine("Press any key to return to categories menu...");
         Console.ReadKey();
         if (!Console.KeyAvailable) GetCategoriesInput();
+    }
+
+    private static T SelectionPrompt<T>(string title, List<T> choices) where T : class
+    {
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<T>()
+                .Title(title)
+                .AddChoices(choices));
     }
 }
