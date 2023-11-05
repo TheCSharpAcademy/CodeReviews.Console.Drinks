7a482028da492510291e85085ff2d5c6601436a6
Matthew J Brown <matthew@lokislayer.com>
2023-11-05 01:12:11 -0600

    completed service for all my api calls

--- Drinks.barakisbrown/Drinks.barakisbrown/DrinksService.cs
+++ Drinks.barakisbrown/Drinks.barakisbrown/DrinksService.cs
@@ -1,24 +1,61 @@
 ﻿using System.Net;
 using Drinks.barakisbrown.Models;
-using drinks_info.Models;
 using Newtonsoft.Json;
 using RestSharp;
 
-public class DrinksService
+namespace Drinks.barakisbrown
 {
-    public IEnumerable<Category> GetCategories()
+    public static class DrinksService
     {
-        var client = new RestClient("http://www.thecocktaildb.com/api/json/v1/1");
-        var request = new RestRequest("list.php?c=list");
-        var response = client.ExecuteAsync(request);
-
-        if (response.Result.StatusCode == HttpStatusCode.OK)
+        private static readonly string clientStr = "http://www.thecocktaildb.com/api/json/v1/1";
+        public static IEnumerable<Category> GetCategories()
         {
-            var rawResponse = response.Result.Content;
-            var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
-            return serialize.CategoriesList;
+            var client = new RestClient(clientStr);
+            var request = new RestRequest("list.php?c=list");
+            var response = client.ExecuteAsync(request);
+
+            if (response.Result.StatusCode == HttpStatusCode.OK)
+            {
+                var rawResponse = response.Result.Content;
+                var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
+                return serialize.CategoriesList;
+            }
+
+            return Enumerable.Empty<Category>();
         }
 
-        return Enumerable.Empty<Category>();
+        public static IEnumerable<Drink> GetDrinkList(Category category)
+        {
+            var requestStr = $"filter.php?c={category.strCategory}";
+
+            var client = new RestClient(clientStr);
+            var request = new RestRequest(requestStr);
+            var response = client.ExecuteAsync(request);
+
+            if (response.Result.StatusCode == HttpStatusCode.OK)
+            {
+                var rawResponse = response.Result.Content;
+                var serialize = JsonConvert.DeserializeObject<Models.Drinks>(rawResponse);
+                return serialize.DrinksList;
+            }
+
+            return Enumerable.Empty<Drink>();
+        }
+
+        public static IEnumerable<DrinkDetail> GetDrink(Drink drink)
+        {
+            var client = new RestClient(clientStr);
+            var request = new RestRequest($"search.php?s={drink.StrDrink}");
+            var response = client.ExecuteAsync(request);
+
+            if (response.Result.StatusCode == HttpStatusCode.OK)
+            {
+                var rawResponse = response.Result.Content;
+                var serialize = JsonConvert.DeserializeObject<Models.DrinkDetailList>(rawResponse);
+                return serialize.DrinksDetail;
+            }
+
+            return Enumerable.Empty<DrinkDetail>();
+        }
     }
 }
