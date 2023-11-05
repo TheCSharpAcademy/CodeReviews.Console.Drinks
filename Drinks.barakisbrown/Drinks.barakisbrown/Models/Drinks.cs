d7eb5716cbb055dea8f778983199509f78223248
Matthew J Brown <matthew@lokislayer.com>
2023-11-05 01:10:29 -0600

    new models convering drink and the drink detail list

--- Drinks.barakisbrown/Drinks.barakisbrown/Models/Drinks.cs
+++ Drinks.barakisbrown/Drinks.barakisbrown/Models/Drinks.cs
@@ -1,15 +1,27 @@
-﻿namespace drinks_info.Models;
+﻿namespace Drinks.barakisbrown.Models;
 
 using Newtonsoft.Json;
+
+public class Drink
+{
+    public Drink()
+    {
+        
+    }
+
+    [JsonProperty("idDrink")]
+    public string? IdDrink { get; set; }
+    [JsonProperty("strDrink")]
+    public string? StrDrink { get; set; }
+
+    public override string ToString()
+    {
+        return $"{StrDrink}";
+    }
+}
 
 public class Drinks
 {
     [JsonProperty("drinks")]
-    public List<Drink> DrinksList { get; set; }
-}
-
-public class Drink
-{
-    public string idDrink { get; set; }
-    public string strSrink { get; set; }
+    public List<Drink>? DrinksList { get; set; }
 }
