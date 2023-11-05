2bb37f5d916ad9fc1e74e3a805852925cbeebc49
Matthew J Brown <matthew@lokislayer.com>
2023-11-05 01:11:33 -0600

    Basic Flow that I need to turn into Loop before turning in to review

--- Drinks.barakisbrown/Drinks.barakisbrown/Program.cs
+++ Drinks.barakisbrown/Drinks.barakisbrown/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello World");
+﻿using Drinks.barakisbrown;
+using Drinks.barakisbrown.Models;
+
+var userInput = new UserInput();
+var Category = userInput.PickCategory();
+var drink = userInput.PickDrink(Category);
+var drinks = DrinksService.GetDrink(drink);
+TableVisualEngine.ShowDrinkInfo(drinks.ToList(),drink.StrDrink);
