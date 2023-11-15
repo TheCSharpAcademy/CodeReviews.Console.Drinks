7eb75aac38b351c97d45ed48f77a5ab556b90258
Matthew J Brown <matthew@lokislayer.com>
2023-11-14 22:02:35 -0600

    Uses Pragma waring to surpress case due to json

--- Drinks.barakisbrown/Drinks.barakisbrown/Models/Category.cs
+++ Drinks.barakisbrown/Drinks.barakisbrown/Models/Category.cs
@@ -5,23 +5,26 @@ using Newtonsoft.Json;
 public class Category
 {
     public Category()
-	{
-	}
+    {
+    }
 
 
 #pragma warning disable IDE1006 // Naming Styles
+
     public string? strCategory { get; set; }
+
 #pragma warning restore IDE1006 // Naming Styles
+
 
     public override string ToString()
     {
-		return $"{strCategory}";
+        return $"{strCategory}";
     }
 }
 
 public class Categories
 {
-	[JsonProperty("drinks")]
-	public List<Category> ?CategoriesList { get; set; }
+    [JsonProperty("drinks")]
+    public List<Category>? CategoriesList { get; set; }
 }
 
