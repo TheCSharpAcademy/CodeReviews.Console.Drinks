04b67d8b621b20ca47b93e945c419cedd45e37ab
Matthew J Brown <matthew@lokislayer.com>
2023-11-14 21:14:49 -0600

    Fixed namespace crap.

--- Drinks.barakisbrown/Drinks.barakisbrown/DrinksService.cs
+++ Drinks.barakisbrown/Drinks.barakisbrown/DrinksService.cs
@@ -1,85 +1,85 @@
-﻿using System.Net;
+﻿namespace Drinks.barakisbrown;
+
+using System.Net;
 using System.Reflection;
 using Drinks.barakisbrown.Models;
 using Newtonsoft.Json;
 using RestSharp;
 
-namespace Drinks.barakisbrown
+
+public static class DrinksService
 {
-    public static class DrinksService
+    private static readonly string clientStr = "http://www.thecocktaildb.com/api/json/v1/1";
+    public static IEnumerable<Category> GetCategories()
     {
-        private static readonly string clientStr = "http://www.thecocktaildb.com/api/json/v1/1";
-        public static IEnumerable<Category> GetCategories()
+        var client = new RestClient(clientStr);
+        var request = new RestRequest("list.php?c=list");
+        var response = client.ExecuteAsync(request);
+
+        if (response.Result.StatusCode == HttpStatusCode.OK)
         {
-            var client = new RestClient(clientStr);
-            var request = new RestRequest("list.php?c=list");
-            var response = client.ExecuteAsync(request);
-
-            if (response.Result.StatusCode == HttpStatusCode.OK)
-            {
-                var rawResponse = response.Result.Content;
-                var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
-                return serialize.CategoriesList;
-            }
-
-            return Enumerable.Empty<Category>();
+            var rawResponse = response.Result.Content;
+            var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
+            return serialize.CategoriesList;
         }
 
-        public static IEnumerable<Drink> GetDrinkByCategory(Category category)
+        return Enumerable.Empty<Category>();
+    }
+
+    public static IEnumerable<Drink> GetDrinkByCategory(Category category)
+    {
+        var requestStr = $"filter.php?c={category.strCategory}";
+
+        var client = new RestClient(clientStr);
+        var request = new RestRequest(requestStr);
+        var response = client.ExecuteAsync(request);
+
+        if (response.Result.StatusCode == HttpStatusCode.OK)
         {
-            var requestStr = $"filter.php?c={category.strCategory}";
-
-            var client = new RestClient(clientStr);
-            var request = new RestRequest(requestStr);
-            var response = client.ExecuteAsync(request);
-
-            if (response.Result.StatusCode == HttpStatusCode.OK)
-            {
-                var rawResponse = response.Result.Content;
-                var serialize = JsonConvert.DeserializeObject<Models.Drinks>(rawResponse);
-                return serialize.DrinksList;
-            }
-
-            return Enumerable.Empty<Drink>();
+            var rawResponse = response.Result.Content;
+            var serialize = JsonConvert.DeserializeObject<Models.Drinks>(rawResponse);
+            return serialize.DrinksList;
         }
 
-        public static List<Object> GetDrink(Drink drink)
+        return Enumerable.Empty<Drink>();
+    }
+
+    public static List<Object> GetDrink(Drink drink)
+    {
+        var client = new RestClient(clientStr);
+        var request = new RestRequest($"search.php?s={drink.StrDrink}");
+        var response = client.ExecuteAsync(request);
+
+        if (response.Result.StatusCode == HttpStatusCode.OK)
         {
-            var client = new RestClient(clientStr);
-            var request = new RestRequest($"search.php?s={drink.StrDrink}");
-            var response = client.ExecuteAsync(request);
+            var rawResponse = response.Result.Content;
+            var serialize = JsonConvert.DeserializeObject<Models.DrinkDetailList>(rawResponse);
+            List<DrinkDetail> returnList = serialize.DrinksDetail;
 
-            if (response.Result.StatusCode == HttpStatusCode.OK)
-            {
-                var rawResponse = response.Result.Content;
-                var serialize = JsonConvert.DeserializeObject<Models.DrinkDetailList>(rawResponse);
-                List<DrinkDetail> returnList = serialize.DrinksDetail;
+            DrinkDetail detail = returnList[0];
+            List<Object> prepList = new();
+            string formattedName = "";
 
-                DrinkDetail detail = returnList[0];
-                List<Object> prepList = new();
-                string formattedName = "";
-
-                foreach (PropertyInfo prop in detail.GetType().GetProperties())
-                { 
-                    if (prop.Name.Contains("str"))
-                    {
-                        formattedName = prop.Name.Substring(3);
-                    }
-
-                    if (!string.IsNullOrEmpty(prop.GetValue(detail)?.ToString())) 
-                    {
-                        prepList.Add(new
-                        {
-                            Key = formattedName,
-                            Value = prop.GetValue(detail)
-                        });
-                    }
+            foreach (PropertyInfo prop in detail.GetType().GetProperties())
+            { 
+                if (prop.Name.Contains("str"))
+                {
+                    formattedName = prop.Name.Substring(3);
                 }
 
-                return prepList;
+                if (!string.IsNullOrEmpty(prop.GetValue(detail)?.ToString())) 
+                {
+                    prepList.Add(new
+                    {
+                        Key = formattedName,
+                        Value = prop.GetValue(detail)
+                    });
+                }
             }
 
-            return Enumerable.Empty<Object>().ToList();
+            return prepList;
         }
+
+        return Enumerable.Empty<Object>().ToList();
     }
 }
