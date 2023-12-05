2348f646ae1c83a1f35f0721a3a03508fd527b20
Álvaro Mosconi <alvaromosconi@outlook.com>
2023-12-05 17:26:06 -0300

    Finished project

---
+++ Drinks.alvaromosconi/DrinksInfo/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DrinksInfo.Models;
+internal record class Category(
+    [property: JsonPropertyName("strCategory")] string Name
+);
+
+internal record class CategoryResponse(
+    [property: JsonPropertyName("drinks")] IEnumerable<Category> Categories
+);
---
+++ Drinks.alvaromosconi/DrinksInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿using DrinksInfo.API;
+using DrinksInfo.Controllers;
+using DrinksInfo.UI;
+using Microsoft.Extensions.DependencyInjection;
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<IDrinksInfoClient, DrinksInfoClient>();
+    services.AddSingleton<IDrinksController, DrinksController>();
+    services.AddSingleton<UserInteface>();
+    services.AddHttpClient("Drinks", client =>
+    {
+        client.BaseAddress = new Uri("http://www.thecocktaildb.com/api/json/v1/1/");
+    });
+}
+
+IServiceCollection _services = new ServiceCollection();
+ConfigureServices(_services);
+IServiceProvider builder = _services.BuildServiceProvider();
+
+async Task InitializeUI()
+{
+    await builder.GetService<UserInteface>().Start();
+}
+
+await InitializeUI();
