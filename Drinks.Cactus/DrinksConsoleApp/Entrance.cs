fbd9999313adddff7be8ad99a63e0c466d34fd7d
Jingwen Xia <amyfor7@gmail.com>
2024-02-25 16:42:37 +0800

    chore: remove unused method parameter

--- Drinks.Cactus/DrinksConsoleApp/Entrance.cs
+++ Drinks.Cactus/DrinksConsoleApp/Entrance.cs
@@ -1,7 +1,7 @@
 ﻿namespace DrinksConsoleApp;
 public class Entrance
 {
-    static async Task Main(string[] args)
+    static async Task Main()
     {
         Application app = new Application();
         await app.Run();
