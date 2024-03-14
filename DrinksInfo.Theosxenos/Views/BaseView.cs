bbc4944145c9646eb332dd5fc7bbfbd18db90540
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-14 16:42:22 +0100

    Remove unused methods

--- DrinksInfo.Theosxenos/Views/BaseView.cs
+++ DrinksInfo.Theosxenos/Views/BaseView.cs
@@ -2,42 +2,10 @@ namespace DrinksInfo.Views;
 
 public class BaseView
 {
-    public void ShowSuccess(string message)
-    {
-        AnsiConsole.MarkupLine($"[green]{message}[/]");
-        Console.ReadKey();
-    }
-
     public void ShowError(string message)
     {
         AnsiConsole.MarkupLine($"[red]{message}[/]");
         Console.ReadKey();
-    }
-
-    public void ShowMessage(string message)
-    {
-        AnsiConsole.MarkupLine(message);
-        Console.ReadKey();
-    }
-
-    public bool AskConfirm(string message)
-    {
-        return AnsiConsole.Confirm(message);
-    }
-
-    public string AskInput(string prompt, string? defaultValue = null)
-    {
-        var textPrompt = new TextPrompt<string>(prompt);
-        if (!string.IsNullOrEmpty(defaultValue)) textPrompt.DefaultValue(defaultValue);
-
-        return AnsiConsole.Prompt(textPrompt);
-    }
-
-    public T AskInput<T>(string prompt, Func<T, bool> validator, string errorMessage)
-    {
-        return AnsiConsole.Prompt(new TextPrompt<T>(prompt)
-            .Validate(validator, errorMessage)
-        );
     }
 
     public T ShowMenu<T>(IEnumerable<T> menuOptions, string title = "Select a menu option:", int pageSize = 10)
