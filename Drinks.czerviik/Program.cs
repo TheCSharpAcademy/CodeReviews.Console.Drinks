a3bc3b5dc6d242a3b18fa28cb8e2c612010e3421
Jiří Malík <czerviik@gmail.com>
2024-04-05 15:33:55 +0200

    delete demo files, start over

--- Drinks.czerviik/Program.cs
+++ Drinks.czerviik/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace drinks_info;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+}
