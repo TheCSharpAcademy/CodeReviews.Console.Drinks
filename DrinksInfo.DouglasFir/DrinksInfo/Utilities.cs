fe1adcdd5a9a5878cc70a9db2ed45b51b926b2ab
Douglas Rank <douglas.rankjr@gmail.com>
2024-05-15 22:28:42 -0700

    project complete

---
+++ DrinksInfo.DouglasFir/DrinksInfo/Utilities.cs
@@ -0,0 +1,12 @@
+﻿namespace DrinksInfo;
+
+public class Utilities
+{
+    public static void PrintNewLines(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine();
+        }
+    }
+}
