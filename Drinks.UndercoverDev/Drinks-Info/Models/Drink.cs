5d50f1f7ce6e410d151de77846e4062fe7026187
N-Endy <okafornel@gmail.com>
2024-06-21 16:46:11 +0100

    Add drink model

--- Drinks.UndercoverDev/Drinks-Info/Models/Drink.cs
+++ Drinks.UndercoverDev/Drinks-Info/Models/Drink.cs
@@ -1,56 +1,16 @@
+using Newtonsoft.Json;
+
 namespace Drinks_Info.Models
 {
     public class Drink
     {
-        public string? StrDrink { get; set; }
-        public object StrDrinkAlternate { get; set; }
-        public object StrTags { get; set; }
-        public object strVideo { get; set; }
-        public string strCategory { get; set; }
-        public object strIBA { get; set; }
-        public string strAlcoholic { get; set; }
-        public string strGlass { get; set; }
-        public string strInstructions { get; set; }
-        public object strInstructionsES { get; set; }
-        public string strInstructionsDE { get; set; }
-        public object strInstructionsFR { get; set; }
-        public string strInstructionsIT { get; set; }
-        public object strInstructionsZHHANS { get; set; }
-        public object strInstructionsZHHANT { get; set; }
-        public string strDrinkThumb { get; set; }
-        public string strIngredient1 { get; set; }
-        public string strIngredient2 { get; set; }
-        public string strIngredient3 { get; set; }
-        public string strIngredient4 { get; set; }
-        public object strIngredient5 { get; set; }
-        public object strIngredient6 { get; set; }
-        public object strIngredient7 { get; set; }
-        public object strIngredient8 { get; set; }
-        public object strIngredient9 { get; set; }
-        public object strIngredient10 { get; set; }
-        public object strIngredient11 { get; set; }
-        public object strIngredient12 { get; set; }
-        public object strIngredient13 { get; set; }
-        public object strIngredient14 { get; set; }
-        public object strIngredient15 { get; set; }
-        public string strMeasure1 { get; set; }
-        public string strMeasure2 { get; set; }
-        public string strMeasure3 { get; set; }
-        public string strMeasure4 { get; set; }
-        public object strMeasure5 { get; set; }
-        public object strMeasure6 { get; set; }
-        public object strMeasure7 { get; set; }
-        public object strMeasure8 { get; set; }
-        public object strMeasure9 { get; set; }
-        public object strMeasure10 { get; set; }
-        public object strMeasure11 { get; set; }
-        public object strMeasure12 { get; set; }
-        public object strMeasure13 { get; set; }
-        public object strMeasure14 { get; set; }
-        public object strMeasure15 { get; set; }
-        public object strImageSource { get; set; }
-        public object strImageAttribution { get; set; }
-        public string strCreativeCommonsConfirmed { get; set; }
-        public string dateModified { get; set; }
+        public int IdDrink { get; set; } = 0;
+        public string StrDrink { get; set; } = "";
+    }
+
+    public class Drinks
+    {
+        [JsonProperty("drinks")]
+        public List<Drink> DrinkList { get; set; } = [];
     }
 }
