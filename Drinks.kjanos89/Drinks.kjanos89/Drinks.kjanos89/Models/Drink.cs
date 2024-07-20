6698762ff5f5a887266d4e9740403da42579ad09
kjanos89 <kjanos89@gmail.com>
2024-07-20 16:54:12 +0200

    Another codacy fix

--- Drinks.kjanos89/Drinks.kjanos89/Drinks.kjanos89/Models/Drink.cs
+++ Drinks.kjanos89/Drinks.kjanos89/Drinks.kjanos89/Models/Drink.cs
@@ -2,7 +2,7 @@
 {
     public class Drink
     {
-        public string idDrink {  get; set; }
-        public string strDrink { get; set; }
+        public string IdDrink {  get; set; }
+        public string StrDrink { get; set; }
     }
 }
