e9c07cc01470ce9a7f4d99b5d888afdc5c5512e4
Shreyas Sawant <90024599+PokingOutsideOFF@users.noreply.github.com>
2024-10-02 23:37:20 +0530

    Project Complete (#86)
    * First commit
    
    * Revert "First commit"
    
    This reverts commit 3e7ac0acad19f14cbb67746f8e6be11f0a3f85e7.
    
    * Project Complete
    
    * Added README
    
    * Fixed some codacy issues
    
    * Fixed some codacy issues

---
+++ KaitoKid.Drinks/DrinksInfo/Models/Drink.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksInfo.Models
+{
+    public class Drink
+    {
+        public string IdDrink {  get; set; }
+        public string StrDrink { get; set; }
+    }
+
+    public class Drinks
+    {
+        [JsonProperty("drinks")]
+        public List<Drink> DrinkList { get; set; }
+    }
+
+}
---
+++ KaitoKid.Drinks/DrinksInfo/Models/DrinkDetail.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksInfo.Models
+{
+    public class DrinkDetail
+    {
+        public string IdDrink { get; set; }
+        public string StrDrink { get; set; }
+        public string StrDrinkAlternate { get; set; }
+        public string StrTags { get; set; }
+        public string StrVideo { get; set; }
+        public string StrCategory { get; set; }
+        public string StrIba { get; set; }
+        public string StrAlcoholic { get; set; }
+        public string StrGlass { get; set; }
+        public string StrInstructions { get; set; }
+        public string StrInstructionsES { get; set; }
+        public string StrInstructionsDE { get; set; }
+        public string StrInstructionsFR { get; set; }
+        public string StrInstructionsIT { get; set; }
+        public string StrInstructionsZH_HANS { get; set; }
+        public string StrInstructionsZH_HANT { get; set; }
+        public string StrDrinkThumb { get; set; }
+        public string StrIngredient1 { get; set; }
+        public string StrIngredient2 { get; set; }
+        public string StrIngredient3 { get; set; }
+        public string StrIngredient4 { get; set; }
+        public string StrIngredient5 { get; set; }
+        public string StrIngredient6 { get; set; }
+        public string StrIngredient7 { get; set; }
+        public string StrIngredient8 { get; set; }
+        public string StrIngredient9 { get; set; }
+        public string StrIngredient10 { get; set; }
+        public string StrIngredient11 { get; set; }
+        public string StrIngredient12 { get; set; }
+        public string StrIngredient13 { get; set; }
+        public string StrIngredient14 { get; set; }
+        public string StrIngredient15 { get; set; }
+        public string StrMeasure1 { get; set; }
+        public string StrMeasure2 { get; set; }
+        public string StrMeasure3 { get; set; }
+        public string StrMeasure4 { get; set; }
+        public string StrMeasure5 { get; set; }
+        public string StrMeasure6 { get; set; }
+        public string StrMeasure7 { get; set; }
+        public string StrMeasure8 { get; set; }
+        public string StrMeasure9 { get; set; }
+        public string StrMeasure10 { get; set; }
+        public string StrMeasure11 { get; set; }
+        public string StrMeasure12 { get; set; }
+        public string StrMeasure13 { get; set; }
+        public string StrMeasure14 { get; set; }
+        public string StrMeasure15 { get; set; }
+        public string StrImageSource { get; set; }
+        public string StrImageAttribution { get; set; }
+        public string StrCreativeCommonsConfirmed { get; set; }
+        public string DateModified { get; set; }
+    }
+
+    public class DrinkDetailObject
+    {
+        [JsonProperty("drinks")]
+        public List<DrinkDetail> DrinkDetailList { get; set; }
+    }
+}
