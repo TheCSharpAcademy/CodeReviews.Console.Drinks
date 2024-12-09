448ee5a96a9a0df9354089b2940a011f9c84d09c
Promise30 <promiseadeagbo@gmail.com>
2024-12-09 08:24:51 +0100

    Updated project files to reflect task solution

---
+++ Promise.DrinksProject/DrinksProject/DrinksProject/Helpers/Validator.cs
@@ -0,0 +1,29 @@
+﻿namespace DrinksProject.Helpers
+{
+    public class Validator
+    {
+        public static bool IsCategoryValid(string category)
+        {
+            if (string.IsNullOrEmpty(category))
+                return false;
+            foreach (char c in category)
+            {
+                if (!char.IsLetter(c) && c != '/' && c != ' ')
+                    return false;
+            }
+            return true;
+        }
+        public static bool IsIdValid(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c)) return false;
+            }
+            return true;
+        }
+
+    }
+
+}
---
+++ Promise.DrinksProject/DrinksProject/DrinksProject/Models/Drinks.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksProject.Models
+{
+    public class Drinks
+    {
+        [JsonProperty("drinks")]
+        public List<Drink> DrinksList { get; set; }
+    }
+    public class Drink
+    {
+        [JsonProperty("idDrink")]
+        public string IdDrink { get; set; }
+        [JsonProperty("strDrink")]
+        public string StrDrink { get; set; }
+
+    }
+
+}
