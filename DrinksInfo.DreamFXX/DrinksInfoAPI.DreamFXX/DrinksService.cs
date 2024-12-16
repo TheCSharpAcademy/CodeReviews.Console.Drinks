5b61a1f4d1e5b6d29903cc2289c5a15019ee6fb8
DreamFXX <filipek.husak@gmail.com>
2024-12-16 02:28:47 +0100

    notes + Visualisation

--- DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/DrinksService.cs
+++ DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/DrinksService.cs
@@ -2,5 +2,9 @@
 
 public class DrinksService
 {
+    public void GetCategories()
+    {
+
+    }
     
 }
--- DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/TableVisualizationEngine.cs
+++ DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/TableVisualizationEngine.cs
@@ -1,6 +1,17 @@
-﻿namespace DrinksInfo.DreamFXX;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DrinksInfo.DreamFXX;
 
 public class TableVisualizationEngine
 {
-    
+    public static void ShowTable<T>(List<T> tableData, [AllowNull] string tableName) where T : class
+    {
+        if (tableName == null) 
+            tableName = "";
+
+        Console.WriteLine("\n\n");
+
+        
+        
+    }
 }
