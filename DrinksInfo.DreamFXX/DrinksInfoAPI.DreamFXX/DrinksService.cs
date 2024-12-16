a3bd07bd7008bf07a89e34bff956f2696789eb5d
DreamFXX <filipek.husak@gmail.com>
2024-12-16 02:41:11 +0100

    added restsharp to the project. gn

--- DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/DrinksService.cs
+++ DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/DrinksService.cs
@@ -1,10 +1,14 @@
-﻿namespace DrinksInfo.DreamFXX;
+﻿using RestSharp;
+
+namespace DrinksInfo.DreamFXX;
 
 public class DrinksService
 {
     public void GetCategories()
     {
-
+        var client = new RestClient("https://www.thecocktaildb.com/api/json/v1/1/");
+        var request = new RestRequest("list.php?c=list");
+        // Continue!
     }
     
 }
--- DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/TableVisualizationEngine.cs
+++ DrinksInfo.DreamFXX/DrinksInfoAPI.DreamFXX/TableVisualizationEngine.cs
@@ -6,6 +6,8 @@ public class TableVisualizationEngine
 {
     public static void ShowTable<T>(List<T> tableData, string? tableName) where T : class
     {
+        Console.Clear();
+
         if (tableName == null) 
             tableName = "";
 
