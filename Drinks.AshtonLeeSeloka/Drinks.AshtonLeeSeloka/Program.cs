34d3069e93a6c8efdb4bdcbda838382c157f9eea
ashtonseloka@outlook.com <ashtonseloka@outlook.com>
2025-01-12 12:37:58 +0200

    initial commit

---
+++ Drinks.AshtonLeeSeloka/Drinks.AshtonLeeSeloka/Program.cs
@@ -0,0 +1,12 @@
+﻿using Drinks.AshtonLeeSeloka.Repositories;
+using Drinks.AshtonLeeSeloka.Services;
+using System.Net.Http.Headers;
+
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+	new MediaTypeWithQualityHeaderValue("application/json"));
+
+DrinksService drinksService = new DrinksService();
+var cat = drinksService.GetCategories(client);
+Console.WriteLine(cat);
---
+++ Drinks.AshtonLeeSeloka/Drinks.AshtonLeeSeloka/Services/DrinksService.cs
@@ -0,0 +1,21 @@
+﻿using Drinks.AshtonLeeSeloka.Repositories;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Net.Http.Headers;
+using System.Text.Json;
+namespace Drinks.AshtonLeeSeloka.Services;
+
+public class DrinksService()
+{
+
+	public async Task<List<Categories>> GetCategories(HttpClient client) 
+	{
+		string? _categories = ConfigurationManager.AppSettings.Get("Categories");
+		await using Stream stream =
+			await client.GetStreamAsync(_categories);
+		var repositories =
+			await JsonSerializer.DeserializeAsync<List<Categories>>(stream);
+		Console.WriteLine(repositories);
+		return repositories ?? new();
+	}
+}
