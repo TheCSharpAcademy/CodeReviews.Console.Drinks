c90add78ed363c64f95cb3623ce56ce9d71717a0
AshtonLeeSeloka <107952769+AshtonLeeSeloka@users.noreply.github.com>
2025-01-22 23:01:22 +0200

    Delete Drinks.AshtonLeeSeloka directory

--- Drinks.AshtonLeeSeloka/Drinks.AshtonLeeSeloka/Repositories/Categories.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Drinks.AshtonLeeSeloka.Repositories;
-
-public record class Categories([property: JsonPropertyName("strCategory")] string Category)
-{
-}
