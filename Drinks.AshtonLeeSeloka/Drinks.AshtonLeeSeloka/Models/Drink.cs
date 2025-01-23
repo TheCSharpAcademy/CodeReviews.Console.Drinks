8cb9f8afc49fc57967a267a0bb074b83ff4294c8
AshtonLeeSeloka <107952769+AshtonLeeSeloka@users.noreply.github.com>
2025-01-23 09:50:36 +0200

    Updated view methods case

---
+++ Drinks.AshtonLeeSeloka/Drinks.AshtonLeeSeloka/Models/Drink.cs
@@ -0,0 +1,41 @@
+﻿namespace HttpRequests.Models;
+
+public class Drink
+{
+	public string? IdDrink { get; set; }
+	public string? DrinkName { get; set; }
+	public string? Category { get; set; }
+	public string? Alcoholic { get; set; }
+	public string? Glass { get; set; }
+	public string? Instructions { get; set; }
+	public string? Ingredient1 { get; set; }
+	public string? Ingredient2 { get; set; }
+	public string? Ingredient3 { get; set; }
+	public string? Ingredient4 { get; set; }
+	public string? Ingredient5 { get; set; }
+	public string? Ingredient6 { get; set; }
+	public string? Ingredient7 { get; set; }
+	public string? Ingredient8 { get; set; }
+	public string? Ingredient9 { get; set; }
+	public string? Ingredient10 { get; set; }
+	public string? Ingredient11 { get; set; }
+	public string? Ingredient12 { get; set; }
+	public string? Ingredient13 { get; set; }
+	public string? Ingredient14 { get; set; }
+	public string? Ingredient15 { get; set; }
+	public string? Measure1 { get; set; }
+	public string? Measure2 { get; set; }
+	public string? Measure3 { get; set; }
+	public string? Measure4 { get; set; }
+	public string? Measure5 { get; set; }
+	public string? Measure6 { get; set; }
+	public string? Measure7 { get; set; }
+	public string? Measure8 { get; set; }
+	public string? Measure9 { get; set; }
+	public string? Measure10 { get; set; }
+	public string? Measure11 { get; set; }
+	public string? Measure12 { get; set; }
+	public string? Measure13 { get; set; }
+	public string? Measure14 { get; set; }
+	public string? Measure15 { get; set; }
+}
---
+++ Drinks.AshtonLeeSeloka/Drinks.AshtonLeeSeloka/Program.cs
@@ -0,0 +1,37 @@
+﻿using HttpRequests.Models;
+using HttpRequests.Repositories;
+using HttpRequests.Services;
+using HttpRequests.Views;
+
+DrinkService _drinkService = new DrinkService();
+DrinkViews _drinkViews = new DrinkViews();
+ValidationService _validationService = new ValidationService();
+
+while (true)
+{
+	Console.Clear();
+
+	
+	List<Drink>? categories = await _drinkService.GetCategoriesAsync();
+	_drinkViews.DisplayCategoriesAsTable(categories);
+	int input = _validationService.UserInput($"Make your selection, only integer's between 0 & {categories.Count} are acceptable\n", categories.Count());
+
+	if (input == 0)
+		Environment.Exit(1);
+
+	string? selectedCategory = categories[(input-1)].Category;
+	List<Drink>? drinks = await _drinkService.GetDrinksAsync(selectedCategory);
+	string selectedDrink = _drinkViews.DisplayDrinks(drinks);
+
+	if (selectedDrink == "exit")
+		continue;
+
+	Drink? drinkDetails = await _drinkService.GetDrinkDetailsAsync(selectedDrink);
+	List<string>? ingredients = _validationService.GetIngredients(drinkDetails);
+	List<string>? measure = _validationService.GetMeasures(drinkDetails);
+	_drinkViews.DisplayDrinkDetails(drinkDetails, ingredients, measure);
+}
+
+
+
+
